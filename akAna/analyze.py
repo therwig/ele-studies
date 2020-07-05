973be59716a957b9691ced13ebbe3e4f9fbdc84e
Dominic Lehner <67029177+Domb32@users.noreply.github.com>
2020-07-05 14:29:04 -0500

    July 4th weekend update. There are range errors that need to be adressed here.

--- akAna/analyze.py
+++ akAna/analyze.py
@@ -41,20 +41,22 @@ def analyze(opts, args):
         "all": reco_electrons.pt > -1,
         "looseMVA": reco_electrons.mvaId > 0,
         "tightMVA": reco_electrons.mvaId > 5,
-        "dominic_special_ip3d"   : ((reco_electrons.sip3d       < 500)    &  
-                                    (reco_electrons.ip3d        < 8)      ),
-        "dominic_special_dxyz"   : ((np.abs(reco_electrons.dz)  < 3)      & 
-                                    (reco_electrons.dzErr       < 0.5)    & 
-                                    (np.abs(reco_electrons.dxy) < 1)      & 
-                                    (reco_electrons.dxyErr      < 0.5)    ),
-        "dominic_special_fBrem"  : reco_electrons.fBrem         > -0.015   ,
-        "dominic_special_Flav"   : reco_electrons.GenPartFlav   > 0.5      , 
-        "dominic_special_mvaId"  : (reco_electrons.mvaId        > -3      ),
-      #                              reco_electrons.mvaId        < 0       ),
-        "dominic_special_pt"     : reco_electrons.pt            > 2        ,
-        "dominic_special_ptBias" : reco_electrons.ptBiased      > 2        ,
-        "dominic_special_trk"    : reco_electrons.trkRelIso     < 10       ,
-        "dominic_special_unBias" : reco_electrons.unBiased      > 2        , 
+        "dominic_special_ip3d"   :((reco_electrons.sip3d       < 1000)   &
+                                   (reco_electrons.ip3d        < 8)      ),
+        "dominic_special_dxyz"   :((np.abs(reco_electrons.dz)  < 0.1)    & 
+                                   (reco_electrons.dzErr       < 0.5)    & 
+                                   (np.abs(reco_electrons.dxy) < 0.05)   & 
+                                   (reco_electrons.dxyErr      < 0.5)    ),
+ #      "dominic_special_fBrem"  :  reco_electrons.fBrem       > - ,
+        "dominic_special_Flav"   :  reco_electrons.GenPartFlav < 0.5     , 
+        "dominic_special_mvaId"  :((reco_electrons.mvaId       > 2.5)    &
+                                   (reco_electrons.mvaId       < 20)     ),
+ #      "dominic_special_pt"     :  reco_electrons.pt          > - ,
+        "dominic_special_ptBias" :((reco_electrons.ptBiased    > 2.5)    &
+                                   (reco_electrons.ptBiased    < 20)     ),
+ #      "dominic_special_trk"    :  reco_electrons.trkRelIso   < - ,
+        "dominic_special_unBias" :((reco_electrons.unBiased    > 0)      &
+                                   (reco_electrons.unBiased    < 5)      ), 
     }
     
     if opts.drawRecoElectrons:
